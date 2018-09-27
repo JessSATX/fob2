ff3b486ae3351a6aa5e61e382247b4cba2aa1237
Joe Anthony Suarez <32373522+AtomicNexus@users.noreply.github.com>
2018-09-27 13:48:30 -0500

    Added Home page (not final)

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
@@ -38,9 +38,11 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(new MyAdapter(
                 toolbar.getContext(),
                 new String[]{
-                        "Section 1",
-                        "Section 2",
-                        "Section 3",
+                        "Home",
+                        "Map",
+                        "Showtimes",
+                        "FAQ",
+                        "About"
                 }));
 
         spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -49,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 // When the given dropdown item is selected, show its contents in the
                 // container view.
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                        .replace(R.id.container, HomeTab.newInstance(position + 1))
                         .commit();
             }
 
@@ -134,22 +136,22 @@ public class MainActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class HomeTab extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public HomeTab() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static HomeTab newInstance(int sectionNumber) {
+            HomeTab fragment = new HomeTab();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
@@ -159,9 +161,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.home_tab, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
