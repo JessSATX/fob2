0ea0852d5dd12cfc774d3aa7f742ef27742753ab
Nathan Marcos <nathanmarcos101@gmail.com>
2018-09-27 07:21:25 -0500

    added expandable list view

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joeanthonysuarez.fiestaoysterbakecompanion;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -62,8 +63,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, Information.class);
+                startActivity(intent);
             }
         });
 
