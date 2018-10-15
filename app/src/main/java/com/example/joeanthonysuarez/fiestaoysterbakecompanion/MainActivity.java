2cb7455f585b783b026badfb146d59be85303bdf
Joe Anthony Suarez <32373522+AtomicNexus@users.noreply.github.com>
2018-10-15 13:32:01 -0500

    Home tab and spinner navigation

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.joeanthonysuarez.fiestaoysterbakecompanion;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -22,12 +20,14 @@ import android.content.res.Resources.Theme;
 
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_tab);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,25 +50,42 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // When the given dropdown item is selected, show its contents in the
                 // container view.
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, HomeTab.newInstance(position + 1))
-                        .commit();
+                if (position == 0) // Home tab content
+                {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, HomeTab.newInstance(position + 1))
+                            .commit();
+                }
+                else if (position == 1) // Map content
+                {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, MapTab.newInstance(position + 1))
+                            .commit();
+                }
+                else if (position == 2) // Showtimes content
+                {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, ShowtimesTab.newInstance(position + 1))
+                            .commit();
+                }
+                else if (position == 3) // About content
+                {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, AboutTab.newInstance(position + 1))
+                            .commit();
+                }
+                else if (position == 4) // FAQ content
+                {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, FAQTab.newInstance(position + 1))
+                            .commit();
+                }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
@@ -161,9 +178,173 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            // currentYear = 3005;   // Play around with different year values.
+
+            String welcomeTextString = "Welcome to the ";
+            welcomeTextString = welcomeTextString + Integer.toString(currentYear);
+            welcomeTextString = welcomeTextString + " Fiesta Oyster Bake!!!";
+
+            int yearOnesDigit = (currentYear - 1916) % 100;
+            String yearCountTextString = "The Fiesta Oyster Bake is celebrating its " + Integer.toString(currentYear - 1916);
+            switch (yearOnesDigit) {
+                case 1:
+                    yearCountTextString = yearCountTextString + "st ";
+                    break;
+                case 2:
+                    yearCountTextString = yearCountTextString + "nd ";
+                    break;
+                case 3:
+                    yearCountTextString = yearCountTextString + "rd ";
+                    break;
+                default:
+                    yearCountTextString = yearCountTextString + "th ";
+                    break;
+            }
+            yearCountTextString = yearCountTextString + "year. The party's just begun at the heartbeat of Fiesta.";
+
             View rootView = inflater.inflate(R.layout.home_tab, container, false);
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            TextView welcomeText = (TextView) rootView.findViewById(R.id.welcome_text);
+            TextView yearCountText = (TextView) rootView.findViewById(R.id.year_count_text);
+            welcomeText.setText(welcomeTextString);
+            yearCountText.setText(yearCountTextString);
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class MapTab extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public MapTab() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static MapTab newInstance(int sectionNumber) {
+            MapTab fragment = new MapTab();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.map_tab, container, false);
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class ShowtimesTab extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public ShowtimesTab() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static ShowtimesTab newInstance(int sectionNumber) {
+            ShowtimesTab fragment = new ShowtimesTab();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.showtimes_tab, container, false);
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class AboutTab extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public AboutTab() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static AboutTab newInstance(int sectionNumber) {
+            AboutTab fragment = new AboutTab();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.about_tab, container, false);
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class FAQTab extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public FAQTab() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static FAQTab newInstance(int sectionNumber) {
+            FAQTab fragment = new FAQTab();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.faq_tab, container, false);
+
             return rootView;
         }
     }
