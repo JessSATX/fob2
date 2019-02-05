d5ff7d29b288744ab05ee2ae76d7d1fe5ab26d1c
Lynntonio <lynntonio.d.r@gmail.com>
2019-02-05 18:56:44 +0000

    Update app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
@@ -61,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Information.class);
