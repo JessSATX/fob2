b4ed449a14797d6a697ca28b1262f48bd1c75209
Joe Anthony Suarez <johony97@gmail.com>
2019-02-12 14:30:08 -0600

    Added some of Jose's login functionality

---
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/ForgotPassword.java
@@ -0,0 +1,62 @@
+package com.example.joeanthonysuarez.fiestaoysterbakecompanion;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    EditText email;
+    Button sendem;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        //FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+        email = (EditText) findViewById(R.id.etEmail);
+        sendem = (Button) findViewById(R.id.bSendEmail);
+
+        sendem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String e_mail = email.getText().toString();
+
+                if (!email.equals(""))
+                {
+                    auth.sendPasswordResetEmail(e_mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(ForgotPassword.this, "An email has been sent to recover your password.", Toast.LENGTH_SHORT).show();
+                                //Intent home = new Intent(ForgotPassword.this, AdminLogin.class);
+                                //startActivity(home);
+                                finish();
+                            } else {
+                                Toast.makeText(ForgotPassword.this, "An error occured. Please verify your email address.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(ForgotPassword.this, "Please enter an e-mail.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MainActivity.java
@@ -247,8 +247,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_admin_login)
+        {
+            Intent intent = new Intent(MainActivity.this, AdminLogin.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
