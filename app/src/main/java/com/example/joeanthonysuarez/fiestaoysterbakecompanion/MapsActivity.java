0de7eb7d6336625720963e31a85e917064685d35
Lynntonio <lynntonio.d.r@gmail.com>
2019-02-12 22:57:41 -0600

    Map Filtering.

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MapsActivity.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/MapsActivity.java
@@ -24,11 +24,19 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This shows how to create a simple activity with a raw MapView and add a marker to it. This
@@ -38,6 +46,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         public static String day;
         private GoogleMap mMap;
+
+        //ArrayList of Marker class, for collecting the references to marker objects. -- Lynntonio
+        List<Marker> markers = new ArrayList<Marker>();
+
 
 
 
@@ -58,6 +70,62 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 day = (String) bundle.get("day");
             }
 
+            ImageButton fbutton = findViewById(R.id.filterButton);
+            // This is the basis to the whole Filter System. ITEMS WILL NOT BE FILTERED IF THEY ARE NOT TAGGED
+            // PROPERLY AND IF THEY ARE NOT STORED IN THE LIST "markers"! -- Lynntonio
+            fbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder alertFilter = new AlertDialog.Builder(MapsActivity.this);
+
+                    //String array for Alert Dialogue multichoice items. MARKER TAGS MUST MATCH ONE OF THE CONTENTS OF THE ARRAY! -- Lynntonio
+                    final String[]  filterArray = new String[] {"Chicken", "Beef", "Seafood", "Veggies", "Sweets", "Beverages", "Alcoholic Beverages", "Other"};
+                    //Boolean Array for selected items. Index contents are meant to correspond with those "filterArray". -- Lynntonio
+                    final boolean[] filterB = new boolean[]    {     true,   true,      true,     true,     true,        true,                  true,     true};
+
+                    alertFilter.setTitle("Select Categories to Filter");
+                    alertFilter.setMultiChoiceItems(filterArray, filterB, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                            filterB[which] = isChecked;
+                        }
+                    });
+
+                    //set positive/Ok button click listener
+                    alertFilter.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // this will check every marker in the markers list and set their visibility
+                            // to match the corresponding filterB's value based on whether the marker's tag
+                            // matches filterArray's current index location. -- Lynntonio
+                            for (int i = 0; i < markers.size(); i++)
+                            {
+                                for (int x = 0; x < filterArray.length; x++)
+                                {
+                                    if (markers.get(i).getTag() == filterArray[x])
+                                    {
+                                            markers.get(i).setVisible(filterB[x]);
+                                    }
+                                }
+                            }
+                        }
+                    });
+
+                    // Set neutral/Cancel button click listener
+                    alertFilter.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    AlertDialog dialog = alertFilter.create();
+                    //show dialog
+                    dialog.show();
+
+
+                }
+            });
         }
 
 
@@ -75,8 +143,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
             mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            mMap.setInfoWindowAdapter(new newinfoAdapter(MapsActivity.this));
+
+            // Most of this is for testing and example purposes, This should be commented out for the final build. -- Lynntonio
             LatLng stmu = new LatLng(29.45249260178782, -98.56478047528071);
-            mMap.addMarker(new MarkerOptions()
+            LatLng c1 = new LatLng(29.45149260178782, -98.56478047528071);
+            LatLng c2 = new LatLng(29.45249260178782, -98.56278047528071);
+            LatLng c3 = new LatLng(29.45349260178782, -98.56278047528071);
+            Marker test1 = mMap.addMarker(new MarkerOptions()
                     .position(stmu)
                     .title("Fiesta Oyster Bake")
                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.food))
@@ -84,11 +158,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                             "Product: Chicken on a stick\n" +
                             "Price: 6 tickets\n" +
                             "Status: Open"));
+            test1.setTag("Chicken");
+            markers.add(test1); // this is one way to store marker references, but requires you to manually initiallize the process. Don't do this.
+            markers.add(mMap.addMarker(new MarkerOptions() // thi is probably the most Ideal way to store markers.
+                    .position(c1)
+                    .title("Test1")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.food))
+                    .snippet("Testing Filter: Tag = 'Beef'")));
+            markers.get(1).setTag("Beef");
+            markers.add(mMap.addMarker(new MarkerOptions()
+                    .position(c2)
+                    .title("Test2")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.food))
+                    .snippet("Testing Filter: Tag = 'Seafood'")));
+            markers.get(2).setTag("Seafood");
+            markers.add(mMap.addMarker(new MarkerOptions()
+                    .position(c3)
+                    .title("Test2")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.food))
+                    .snippet("Testing Filter: Tag = 'Seafood'")));
+            markers.get(3).setTag("Seafood");
+            // end test/example -- Lynntonio
+            // Note: Most likely won't be an issue, however Tags need to be released by setting them to NULL, in order to prevent memory leaks.
+            // Deleting the marker does not release the tag and instead loses the reference to the tag. Should't see negative consequences
+            // unless we start making thousands of tags, in which case we need to start releasing tags. -- Lynntonio
+
+            //A statement like this will work to hide markers in the[]:  markers.get(0).setVisible(false);
+            // get tag will also work
             mMap.moveCamera(CameraUpdateFactory.newLatLng(stmu));
             mMap.getUiSettings().setZoomControlsEnabled(true);
             // Move the camera instantly to stmu with a zoom of 15.
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stmu, 15));
-
             if(day.equals("1")) {
                 //this is all shaky and the map is retarded...fix it
                 GroundOverlayOptions fridayMap = new GroundOverlayOptions()
