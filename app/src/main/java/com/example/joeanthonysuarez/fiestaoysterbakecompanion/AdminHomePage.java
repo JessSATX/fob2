3c69bcd5e512426f52b9dfa90b0ec2ffc1897abe
jtanori7 <37081578+jtanori7@users.noreply.github.com>
2019-02-26 17:16:24 -0600

    second change

---
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/AdminHomePage.java
@@ -0,0 +1,13 @@
+package com.example.joeanthonysuarez.fiestaoysterbakecompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home_page2);
+    }
+}
