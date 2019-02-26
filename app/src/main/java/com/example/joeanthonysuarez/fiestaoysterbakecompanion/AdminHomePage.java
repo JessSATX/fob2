b4eb323add51847b8f4ad606a245cc25f51d802a
jtanori7 <37081578+jtanori7@users.noreply.github.com>
2019-02-26 17:17:44 -0600

    second change

--- app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/AdminHomePage.java
+++ app/src/main/java/com/example/joeanthonysuarez/fiestaoysterbakecompanion/AdminHomePage.java
@@ -1,13 +1,41 @@
 package com.example.joeanthonysuarez.fiestaoysterbakecompanion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminHomePage extends AppCompatActivity {
-
+    Button bmap, bshowtime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home_page2);
+        setContentView(R.layout.activity_admin_home_page);
+
+        bmap = (Button) findViewById(R.id.bMaps);
+        bshowtime = (Button) findViewById(R.id.bShowtimes);
+
+        bmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoSelectdate = new Intent(AdminHomePage.this, DateSelect.class);
+
+                gotoSelectdate.putExtra("Type", "maps");
+
+                startActivity(gotoSelectdate);
+            }
+        });
+
+        bshowtime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoSelectdate = new Intent(AdminHomePage.this, DateSelect.class);
+
+                gotoSelectdate.putExtra("Type", "showtimes");
+
+                startActivity(gotoSelectdate);
+            }
+        });
     }
 }
